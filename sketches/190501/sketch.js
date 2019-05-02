e846c4b3bd5b7ef435260bf1c50b31a027432b9a
GGERKK <amygerken@Amys-MacBook-Pro.local>
2019-05-01 22:50:20 -0400

    Update 190501

--- sketches/190501/sketch.js
+++ sketches/190501/sketch.js
@@ -1,26 +1,41 @@
 // "use strict"
-let diameter
-let angle = 0
-const lowEnd = -150
-const highEnd = 150
 
 function setup() {
-    createCanvas(windowWidth, windowHeight)
-    diameter = 150
-    frameRate(60)
-    rectMode(CENTER)
-    background(19, 22, 23)
+  createCanvas(1080, 1080)
+  frameRate(60)
+  rectMode(CENTER)
+  background(0)
 
 }
 
 function draw() {
 
+  push()
     translate(width/2, height/2)
     stroke(0, 0)
-    fill(195, 0, 185)
+    fill(255)
     ellipse(0, 0, 400, 160)
     fill(0, 0)
-    stroke(255)
-    ellipse(0, 50, 160, 160)
+    stroke(0)
+    ellipse(0, 0, 140, 140)
+  pop()
 
+  push()
+    translate(mouseX, mouseY)
+    stroke(255)
+    fill(0)
+    ellipse(0, 0, 20, 20)
+  pop()
+
+}
+
+// function mousePressed() {
+//   background(0)
+// }
+
+function keyPressed() {
+  // tab key
+  if (keyCode === TAB) {
+    saveCanvas('190501', 'png')
+  }
 }
