7e65155ff19a9c672e2c8e058f18737b67c32061
GGERKK <amygerken@Amys-MacBook-Pro.local>
2019-08-31 22:24:32 -0400

    New sketches

---
+++ sketches/190831 P_1_1_2_01/sketch.js
@@ -0,0 +1,68 @@
+
+
+/*
+Generative Design :  Color
+*/
+
+/*
+ * changing the color circle by moving the mouse.
+
+ * MOUSE
+ * position x          : saturation
+ * position y          : brighness
+
+ * KEYS
+ * 1-5                 : number of segments
+ * s                   : save png
+ */
+
+'use strict'
+
+var segmentCount = 12
+var radius = 300
+
+function setup() {
+  createCanvas(800, 800)
+  noStroke()
+}
+
+function draw() {
+  colorMode(HSB, 360, width, height)
+  background(360, 0, height)
+
+  let angleStep = 360 / segmentCount
+
+  beginShape(TRIANGLE_FAN)
+  vertex(width/2, height/2)
+
+  for(let angle = 0; angle <= 360; angle += angleStep) {
+    let vx = width/2 + cos(radians(angle)) * radius
+    let vy = height/2 + sin(radians(angle)) * radius
+    vertex(vx, vy)
+    fill(angle, mouseX, mouseY)
+  }
+
+  endShape()
+}
+
+function keyPressed() {
+  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png')
+
+  switch (key) {
+    case '1':
+      segmentCount = 360
+      break
+    case '2':
+      segmentCount = 45
+      break
+    case '3':
+      segmentCount = 24
+      break
+    case '4':
+      segmentCount = 12
+      break
+    case '5':
+      segmentCount = 6
+      break
+  }
+}
